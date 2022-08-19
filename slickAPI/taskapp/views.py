ed371e76bab6041059741078f1b5369045ee1488
psalmuelle <bjoestores@gmail.com>
2022-08-19 18:47:24 +0100

    initialized todoapp views

--- slickAPI/taskapp/views.py
+++ slickAPI/taskapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Todo, TodoList
+from .serializers import TodoListSerializer, TodoSerializer
 
-# Create your views here.
+#Todo Views 
+
+@api_view(["POST"])
+def create_todo(request):
+    pass
+
+
+@api_view(["GET"])
+def get_todo(request):
+    pass
+
+
+@api_view(["DELETE"])
+def delete_todo(request):
+    pass
+
+
+#TodoList views
+
+@api_view(["POST"])
+def create_todolist(request):
+    pass
+
+
+@api_view(["GET"])
+def get_todolist(request):
+    pass
+
+
+
+@api_view(["PATCH"])
+def update_todolist(request):
+    pass
