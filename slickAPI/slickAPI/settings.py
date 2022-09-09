4649c73bfe1586ad7b0e543b280a369b7de55cea
psalmuelle <bjoestores@gmail.com>
2022-09-09 13:58:50 +0100

    using pythonanywhere

--- slickAPI/slickAPI/settings.py
+++ slickAPI/slickAPI/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(","))
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -148,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/static'
-STATIC_ROOT = "/vol/web/static"
+STATIC_URL = 'static/'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # new
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
